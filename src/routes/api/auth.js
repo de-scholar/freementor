670f5521ba5a4b3d60991af9cc1c4f1267c711fb
kakaprodo <promskakaprodo45@gmail.com>
2019-08-20 11:49:53 +0200

    ft(data-validation):add auth-validation [Finishes #167996719]

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import userValidation from '../../middleware/userValidation';
+import validate from '../../middleware/validate';
 import AuthController from '../../controller/AuthController';
 import bodyParser from 'body-parser';
 
@@ -8,10 +10,15 @@ const urlEncodedParser=bodyParser.urlencoded({extended:false});
 
 router.post('/auth/signup',
   urlEncodedParser,
+  userValidation.signup,
+  validate,
   AuthController.signUp);
 
 router.post('/auth/signin',
   urlEncodedParser,
+  userValidation.signin,
+  validate,
   AuthController.signIn);
+
 
 export default router;
