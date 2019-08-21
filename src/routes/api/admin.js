0d7e8e58813ed8737f9a22e970f1f4e2876dad31
kakaprodo <promskakaprodo45@gmail.com>
2019-08-21 11:01:38 +0200

    ft(mentorship-session): accept sesion [Finishes #168023252]

--- src/routes/api/admin.js
+++ src/routes/api/admin.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import AdminMw from '../../middleware/admin';
+import permissionMw from '../../middleware/permission';
 import AdminController from '../../controller/AdminController';
 import AuthMw from '../../middleware/auth';
 
 const router = express.Router();
 const {authorization,tokenVerify}=AuthMw;
-const {isAdmin}=AdminMw;
+const {isAdmin}=permissionMw;
 
 /* User to mentor*/
 router.patch('/user/:userId',
