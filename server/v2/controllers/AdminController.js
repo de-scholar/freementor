f818f8f5ad3e4d908d532a12eb984b87abbd7633
kakaprodo <promskakaprodo45@gmail.com>
2019-09-08 11:43:03 +0200

    ft(db): user to admin  #168362026

---
+++ server/v2/controllers/AdminController.js
@@ -0,0 +1,20 @@
+import User from '../models/User';
+import GeneralHelper from '../helpers/general';
+
+const { response } = GeneralHelper;
+
+class AdminController {
+  static userToAdmin(req, res, next) {
+    const { userId } = req.params;
+
+    return User.switchTo(userId, { role: 'admin' }, res, next);
+  }
+
+  static adminToUser(req, res, next) {
+    const { userId } = req.params;
+
+    return User.switchTo(userId, { role: 'user' }, res, next);
+  }
+}
+
+export default AdminController;
---
+++ server/v2/routes/api/admin.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import permissionMw from '../../middleware/permission';
+import AdminController from '../../controllers/AdminController';
+import AuthMw from '../../middleware/auth';
+
+const router = express.Router();
+const { authorization, tokenVerify } = AuthMw;
+const { isAdmin } = permissionMw;
+
+
+router.patch('/admin/:userId',
+  authorization,
+  tokenVerify,
+  AdminController.userToAdmin);
+
+
+router.patch('/admin-to/:userId/',
+  authorization,
+  tokenVerify,
+  isAdmin,
+  AdminController.adminToUser);
+
+
+export default router;
