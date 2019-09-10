43b0a2616384cedc480a89c2ae54619eb14cef02
kakaprodo <promskakaprodo45@gmail.com>
2019-09-11 00:24:06 +0200

    ch(herolu) setup a production env [Finishes #168418816]

--- server/v2/models/db.js
+++ server/v2/models/db.js
@@ -1,6 +1,11 @@
 import pg, { Pool } from 'pg';
 import config from '../config/config';
+const { NODE_ENV, DATABASE_URL } = process.env;
+let connection = config.dbConfig;
 
-const db = new Pool(config.dbConfig);
+if(NODE_ENV === 'production'){
+	const  connection = {connectionString: DATABASE_URL};
+}
+const db = new Pool(connection);
 
 export default db;
